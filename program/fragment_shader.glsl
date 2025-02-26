f81f4c438a75c65993ab846dc3d60dea6e3ed3e7
Bonnie54875 <115943510+Bonnie54875@users.noreply.github.com>
2025-02-26 11:34:36 +0200

    Update fragment_shader.glsl

--- program/fragment_shader.glsl
+++ program/fragment_shader.glsl
@@ -33,15 +33,6 @@ vec2 fOpUnionID(vec2 res1, vec2 res2) {
     return (res1.x < res2.x) ? res1 : res2;
 }
 
-vec2 fOpDiffirenceID(vec2 res1, vec2 res2) {
-    return (res1.x > -res2.x) ? res1 : vec2(-res2.x, res2.y);
-}
-
-vec2 fOpDifferenceColumnsID(vec2 res1, vec2 res2, float r, float n) {
-    float dist = fOpDifferenceColumns(res1.x, res2.x, r, n);
-    return (res1.x > -res2.x) ? vec2(dist, res1.y) : vec2(dist, res2.y);
-}
-
 #include map.glsl
 #include material.glsl
 
